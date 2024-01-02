0ec92c84ae9e2c64ced44ee9d2cdd2845c4b52ce
vaibhavps05 <vaibhavps3005@gmail.com>
2024-01-03 03:20:39 +0530

    passport auth

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const bcrypt = require("bcrypt");
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
 
+const initializePassport = require('./passport-config');
+initializePassport(
+  passport,
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+);
+
+const users = [];
 
 const app = express();
 
@@ -9,8 +25,16 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.use(flash());
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get("/", function(req,res){
+app.get("/", checkAuthenticated, function(req,res){
     res.render("home");
   });
 
@@ -35,8 +59,18 @@ app.get("/", function(req,res){
   });
 
 
-  app.get("/login", function(req,res){
+  app.get("/login", checkNotAuthenticated, function(req,res){
     res.render("login");
+  });
+
+
+  app.get("/registerC", function(req,res){
+    res.render("registerC");
+  });
+
+
+  app.get("/registerL", checkNotAuthenticated, function(req,res){
+    res.render("registerL");
   });
 
 
@@ -44,8 +78,46 @@ app.get("/", function(req,res){
     res.render("family");
   });
 
- 
 
+  app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+  }));
+
+
+  app.post("/registerL", checkNotAuthenticated, async (req,res) => {
+    try{
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      users.push({
+        id: Date.now().toString(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+      });
+      res.redirect("/login");
+    }
+    catch{
+      res.redirect("/registerL");
+    }
+    console.log(users);
+  });
+
+ 
+  function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+  
+    res.redirect('/login');
+  }
+  
+  function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect('/');
+    }
+    next();
+  }
 
  
 app.listen(3000, function() {
