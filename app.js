a8ca7bbd0192c85d799d7d6d057d1024499412ed
vaibhavps05 <vaibhavps3005@gmail.com>
2024-01-25 23:38:25 +0530

    format-ing

--- app.js
+++ app.js
@@ -1,69 +1,70 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const bcrypt = require("bcrypt");
-const passport = require('passport');
-const flash = require('express-flash');
-const session = require('express-session');
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
 
-const initializePassport = require('./passport-config');
+const initializePassport = require("./passport-config");
 initializePassport(
   passport,
-  email => users.find(user => user.email === email),
-  id => users.find(user => user.id === id)
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
 );
 
 const users = [];
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(flash());
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', require('./routes/index'));
-app.use('/', require('./routes/users'));
+app.use("/", require("./routes/index"));
+app.use("/", require("./routes/users"));
 
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
 
-  app.post('/login', passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true
-  }));
+app.post("/registerL", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    res.redirect("/login");
+    return;
+  } catch {
+    res.redirect("/registerL");
+  }
+  console.log(users);
+});
 
-
-  app.post("/registerL", async (req,res) => {
-    try{
-      const hashedPassword = await bcrypt.hash(req.body.password, 10);
-      users.push({
-        id: Date.now().toString(), 
-        name: req.body.name,
-        email: req.body.email,
-        password: hashedPassword
-      });
-      res.redirect("/login");
-      return
-    }
-    catch{
-      res.redirect("/registerL");
-    }
-    console.log(users);
-  });
-
- 
-app.listen(3000, function() {
-    console.log("Server started on port 3000");
-  });
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
