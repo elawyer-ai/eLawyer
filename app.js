271b10e361717f0e7d548b689836036db5fc4fb7
vaibhavps05 <vaibhavps3005@gmail.com>
2024-03-03 01:11:51 +0530

    db bugs solved

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const lodash = require("lodash");
 const mongoose = require("mongoose");
 
 require("./passport-config")(passport);
+const User = require('./models/User');
+const Lawyer = require('./models/Lawyer');
 
 
 
@@ -102,6 +104,152 @@ app.get("/post/:topic", function(req,res){
 
   });
 
+});
+
+
+app.post('/registerC', (req, res) => {
+  const { name, email, password, phone, pincode } = req.body;
+  let errors = [];
+
+  if (!name || !email || !password || !phone || !pincode) {
+    errors.push({ msg: 'Please enter all fields' });
+  }
+
+  if (password.length < 6) {
+    errors.push({ msg: 'Password must be at least 6 characters' });
+  }
+
+  if (phone.length > 10) {
+    errors.push({ msg: 'Enter valid Phone' });
+  }
+  
+  if (pincode.length > 7) {
+    errors.push({ msg: 'Enter valid Pincode' });
+  }
+
+  if (errors.length > 0) {
+    res.render('registerC', {
+      errors,
+      name,
+      email,
+      password,
+      phone,
+      pincode
+    });
+  } else {
+    User.findOne({ email: email }).then(user => {
+      if (user) {
+        errors.push({ msg: 'Email already exists' });
+        res.render('registerC', {
+          errors,
+          name,
+          email,
+          password,
+          phone,
+          pincode
+        });
+      } else {
+        const newUser = new User({
+          name,
+          email,
+          password,
+          phone,
+          pincode
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => {
+                req.flash(
+                  'success_msg',
+                  'You are now registered and can log in'
+                );
+                res.redirect('/login');
+              })
+              .catch(err => console.log(err));
+          });
+        });
+      }
+    });
+  }
+})
+
+
+app.post("/registerL", async (req, res) => {
+
+  // const data = {
+  //     email: req.body.email,
+  //     name: req.body.name,
+  //     password: req.body.password,
+  //     phone: req.body.phone,
+  //     copnum: req.body.copnum
+  // }
+
+  const { name, email, password, phone, copnum } = req.body;
+
+  const existingUser = await Lawyer.findOne({ email: email });
+
+  if (existingUser) {
+      res.send('User already exists. Please choose a different username.');
+  } else {
+
+    const newLawyer = new Lawyer({
+      name,
+      email,
+      password,
+      phone,
+      copnum
+    });
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newLawyer.password, salt, (err, hash) => {
+        if (err) throw err;
+        newLawyer.password = hash;
+        newLawyer
+          .save()
+          .then(user => {
+            req.flash(
+              'success_msg',
+              'You are now registered and can log in'
+            );
+            res.redirect('/lawyer');
+          })
+          .catch(err => console.log(err));
+      });
+    });
+      // const saltRounds = 10;
+      // const hashedPassword = await bcrypt.hash(data.password, saltRounds);
+
+      // data.password = hashedPassword;
+
+      // const userdata = await Lawyer.insertMany(data);
+      // console.log(userdata);
+      // res.redirect('/lawyer');
+  }
+
+});
+
+app.post("/lawyer", async (req, res) => {
+  try {
+      const check = await Lawyer.findOne({ email: req.body.email });
+      if (!check) {
+          res.send("User name cannot found")
+      }
+      const isPasswordMatch = await bcrypt.compare(req.body.password, check.password);
+      if (!isPasswordMatch) {
+          res.send("wrong Password");
+      }
+      else {
+          res.redirect('/userLawyer');
+      }
+  }
+  catch {
+      res.send("wrong Details");
+  }
 });
 
 
--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,8 @@ const passport = require('passport');
 
 
 
-const User = require('../models/User')
-const Lawyer = require('../models/Lawyer')
+// const User = require('../models/User')
+// const Lawyer = require('../models/Lawyer')
 
 
 
@@ -39,123 +39,123 @@ router.get('/registerC', forwardAuthenticated, (req, res) => res.render('registe
 
 
 
-  router.post('/registerC', (req, res) => {
-    const { name, email, password, phone, pincode } = req.body;
-    let errors = [];
+  // router.post('/registerC', (req, res) => {
+  //   const { name, email, password, phone, pincode } = req.body;
+  //   let errors = [];
   
-    if (!name || !email || !password || !phone || !pincode) {
-      errors.push({ msg: 'Please enter all fields' });
-    }
+  //   if (!name || !email || !password || !phone || !pincode) {
+  //     errors.push({ msg: 'Please enter all fields' });
+  //   }
   
-    if (password.length < 6) {
-      errors.push({ msg: 'Password must be at least 6 characters' });
-    }
+  //   if (password.length < 6) {
+  //     errors.push({ msg: 'Password must be at least 6 characters' });
+  //   }
   
-    if (phone.length > 10) {
-      errors.push({ msg: 'Enter valid Phone' });
-    }
+  //   if (phone.length > 10) {
+  //     errors.push({ msg: 'Enter valid Phone' });
+  //   }
     
-    if (pincode.length > 7) {
-      errors.push({ msg: 'Enter valid Pincode' });
-    }
+  //   if (pincode.length > 7) {
+  //     errors.push({ msg: 'Enter valid Pincode' });
+  //   }
   
-    if (errors.length > 0) {
-      res.render('registerC', {
-        errors,
-        name,
-        email,
-        password,
-        phone,
-        pincode
-      });
-    } else {
-      User.findOne({ email: email }).then(user => {
-        if (user) {
-          errors.push({ msg: 'Email already exists' });
-          res.render('registerC', {
-            errors,
-            name,
-            email,
-            password,
-            phone,
-            pincode
-          });
-        } else {
-          const newUser = new User({
-            name,
-            email,
-            password,
-            phone,
-            pincode
-          });
+  //   if (errors.length > 0) {
+  //     res.render('registerC', {
+  //       errors,
+  //       name,
+  //       email,
+  //       password,
+  //       phone,
+  //       pincode
+  //     });
+  //   } else {
+  //     User.findOne({ email: email }).then(user => {
+  //       if (user) {
+  //         errors.push({ msg: 'Email already exists' });
+  //         res.render('registerC', {
+  //           errors,
+  //           name,
+  //           email,
+  //           password,
+  //           phone,
+  //           pincode
+  //         });
+  //       } else {
+  //         const newUser = new User({
+  //           name,
+  //           email,
+  //           password,
+  //           phone,
+  //           pincode
+  //         });
   
-          bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(newUser.password, salt, (err, hash) => {
-              if (err) throw err;
-              newUser.password = hash;
-              newUser
-                .save()
-                .then(user => {
-                  req.flash(
-                    'success_msg',
-                    'You are now registered and can log in'
-                  );
-                  res.redirect('/login');
-                })
-                .catch(err => console.log(err));
-            });
-          });
-        }
-      });
-    }
-  })
+  //         bcrypt.genSalt(10, (err, salt) => {
+  //           bcrypt.hash(newUser.password, salt, (err, hash) => {
+  //             if (err) throw err;
+  //             newUser.password = hash;
+  //             newUser
+  //               .save()
+  //               .then(user => {
+  //                 req.flash(
+  //                   'success_msg',
+  //                   'You are now registered and can log in'
+  //                 );
+  //                 res.redirect('/login');
+  //               })
+  //               .catch(err => console.log(err));
+  //           });
+  //         });
+  //       }
+  //     });
+  //   }
+  // })
 
 
-  router.post("/registerL", async (req, res) => {
+//   router.post("/registerL", async (req, res) => {
 
-    const data = {
-        email: req.body.email,
-        name: req.body.name,
-        password: req.body.password,
-        phone: req.body.phone,
-        copnum: req.body.copnum
-    }
+//     const data = {
+//         email: req.body.email,
+//         name: req.body.name,
+//         password: req.body.password,
+//         phone: req.body.phone,
+//         copnum: req.body.copnum
+//     }
 
-    const existingUser = await Lawyer.findOne({ email: data.email });
+//     const existingUser = await Lawyer.findOne({ email: data.email });
 
-    if (existingUser) {
-        res.send('User already exists. Please choose a different username.');
-    } else {
-        const saltRounds = 10;
-        const hashedPassword = await bcrypt.hash(data.password, saltRounds);
+//     if (existingUser) {
+//         res.send('User already exists. Please choose a different username.');
+//     } else {
+//         const saltRounds = 10;
+//         const hashedPassword = await bcrypt.hash(data.password, saltRounds);
 
-        data.password = hashedPassword;
+//         data.password = hashedPassword;
 
-        // const userdata = await Lawyer.insertMany(data);
-        // console.log(userdata);
-        res.redirect('/lawyer');
-    }
+//         // const userdata = await Lawyer.insertMany(data);
+//         // console.log(userdata);
+//         res.redirect('/lawyer');
+//     }
 
-});
+// });
 
-router.post("/lawyer", async (req, res) => {
-    try {
-        const check = await Lawyer.findOne({ email: req.body.email });
-        if (!check) {
-            res.send("User name cannot found")
-        }
-        const isPasswordMatch = await bcrypt.compare(req.body.password, check.password);
-        if (!isPasswordMatch) {
-            res.send("wrong Password");
-        }
-        else {
-            res.redirect('/userLawyer');
-        }
-    }
-    catch {
-        res.send("wrong Details");
-    }
-});
+// router.post("/lawyer", async (req, res) => {
+//     try {
+//         const check = await Lawyer.findOne({ email: req.body.email });
+//         if (!check) {
+//             res.send("User name cannot found")
+//         }
+//         const isPasswordMatch = await bcrypt.compare(req.body.password, check.password);
+//         if (!isPasswordMatch) {
+//             res.send("wrong Password");
+//         }
+//         else {
+//             res.redirect('/userLawyer');
+//         }
+//     }
+//     catch {
+//         res.send("wrong Details");
+//     }
+// });
 
   router.get('/logout', (req, res) => {
     req.logout(function(err) {
